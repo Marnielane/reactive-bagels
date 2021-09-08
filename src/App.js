9bde395e6e5785803462bad7cdc63800c3b52f34
Marnie Robinson <marnielane@gmail.com>
2021-09-08 13:32:54 -0600

    setup appJs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import BagelsContainer from './components/BagelsContainer'
+import Form from './components/Form'
 
 function App() {
   return (
-    <h1>This is our Bagels App</h1>
+    <>
+      <h1>This is our Bagels App</h1>
+      <Form/>
+      <BagelsContainer/>
+    </>
   );
 }
 
