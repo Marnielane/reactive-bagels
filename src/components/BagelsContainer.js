3376c610e17dfe1f47d47ada1d8a335618c83da9
Marnie Robinson <marnielane@gmail.com>
2021-09-08 13:44:18 -0600

    setup bagelscontainer relationships

--- src/components/BagelsContainer.js
+++ src/components/BagelsContainer.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Delete from './Delete'
+import Bagel from './Bagel'
 
-const BagelsContainer =() =>{
-    return <h3>This is the Bagels Container</h3>
+const BagelsContainer = () => {
+    return (
+        <>
+        <h3>This is the Bagels Container</h3>
+        <div>
+            <Delete/>
+            <Bagel/>
+        </div>
+        </>
+    )
 }
 
 export default BagelsContainer
