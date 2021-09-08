7f513f767685f9ed16724b2d4665b53e1931dc84
Marnie Robinson <marnielane@gmail.com>
2021-09-08 14:16:40 -0600

    setup class component app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 import BagelsContainer from './components/BagelsContainer'
 import Form from './components/Form'
 
-function App() {
-  return (
-    <>
-      <h1>This is our Bagels App</h1>
-      <Form/>
-      <BagelsContainer/>
-    </>
-  );
+class App extends React.Component {
+
+  state = {
+    bagels:['bagel 1', 'bagel 2']
+  }
+  
+  render(){
+    return (
+      <>
+        <h1>This is our Bagels App</h1>
+        <Form/>
+        <BagelsContainer/>
+      </>
+    );
+  }
 }
 
 export default App;
